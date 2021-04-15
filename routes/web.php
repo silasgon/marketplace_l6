16cddfaaff8e957fd8063085cbf3ecaf5a766ca9
Silas Gonçalves <silasidc@gmail.com>
2021-04-15 18:26:52 -0300

    models casta and hidden

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,37 @@ Route::get('/', function () {
 });
 
 Route::get('/model', function(){
-    $products = \App\Product::all();
-    return $products;
+    //$products = \App\Product::all();
+    
+/*     $user = new \App\User();
+    
+    $user->name = 'Usuario teste 2';
+    $user->email = 'email2@test.com';
+    $user->password = bcrypt('12345678');
+
+    
+   $user->save(); */
+   //return \App\User::find(3);
+   
+   //return \App\User::where('name', 'Usuario teste 2')->get();
+
+  // return \App\User::paginate(10);
+/* 
+  $user = \App\User::create([
+      'name' => 'Silas2 Reis',
+      'email'=> 'silas2@silas.com',
+      'password' => bcrypt('123456')
+  ]);
+ */
+  //dd($user);
+/*   $user = \App\User::find(46);
+  $user->update([
+    'name' => 'Atuaizando com mass update'  
+  ]);
+   */
+  return\App\User::all();
 });
 
-Auth::routes();
+/* Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home'); */
