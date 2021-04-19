f37dc1fdb32a3d59f5a249c4b86dd70dab33516a
Silas Gonçalves <silasidc@gmail.com>
2021-04-19 18:44:23 -0300

    relations laravel

--- database/migrations/2021_04_14_031312_create_products_table.php
+++ database/migrations/2021_04_14_031312_create_products_table.php
@@ -19,7 +19,7 @@ class CreateProductsTable extends Migration
             
             $table->string('name');
             $table->string('description');
-            $table->string('body');
+            $table->text('body');
             $table->decimal('price', 10, 2);
             $table->string('slug');
             
